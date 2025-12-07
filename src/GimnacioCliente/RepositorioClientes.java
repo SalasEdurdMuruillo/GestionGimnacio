dffdfd3d3ef1ad2d5e1cab0ecc03f4baf6e2de33
Felipe0220 <lusalaspe@est.utn.ac.cr>
2025-12-07 12:29:00 -0600

    Clase RepositorioCliente
    Se creo la clase RepositorioCliente que usa una interfaz y extiende de RepositorioGenerico datos Cliente, String

---
+++ src/GimnacioCliente/RepositorioClientes.java
@@ -0,0 +1,10 @@
+package GimnacioCliente;
+
+import GimnacioInfraestructura.RepositorioGenerico;
+/**
+ *
+ * @author Luisf
+ */
+
+public interface RepositorioClientes extends RepositorioGenerico<Cliente, String> {
+}
