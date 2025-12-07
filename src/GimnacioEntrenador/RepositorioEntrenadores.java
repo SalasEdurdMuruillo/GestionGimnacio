dd0f5bf5c7e12043cba1159ca02010059274067a
eduardsalas <edusalasmurqest.utn.ac.cr>
2025-12-07 13:05:05 -0600

    Interfaz RepositorioEntrenadores
    Se creo la intefaz RepositorioEntrenadores que establece el contrato específico para la gestión de la persistencia de la entidad Entrenador dentro del módulo GimnacioEntrenador

---
+++ src/GimnacioEntrenador/RepositorioEntrenadores.java
@@ -0,0 +1,10 @@
+package GimnacioEntrenador;
+
+import GimnacioInfraestructura.RepositorioGenerico;
+
+/**
+ *
+ * @author Eduard Salas Murillo
+ */
+public interface RepositorioEntrenadores extends RepositorioGenerico<Entrenador, String> {
+}
