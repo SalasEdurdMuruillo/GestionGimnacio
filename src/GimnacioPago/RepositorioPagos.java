3bbbd60de82c40062a334b6bf27cd7fd23bb0fb4
eduardsalas <edusalasmurqest.utn.ac.cr>
2025-12-07 13:58:01 -0600

    Interfaz RepositorioPagos
    Se creo la interfaz RepositorioPagos para manejar el flujo de registro de pagos y centraliza la lógica de negocio usando el patrón Strategy para definir el cálculo del monto final y el patrón Observer para notificar a otros módulos después de un registro exitoso y por ultimo se apoya en RepositorioPagos para la persistencia de datos CRUD y consultas

---
+++ src/GimnacioPago/RepositorioPagos.java
@@ -0,0 +1,10 @@
+package GimnacioPago;
+
+import GimnacioInfraestructura.RepositorioGenerico;
+
+/**
+ *
+ * @author Eduard Salas Murillo
+ */
+public interface RepositorioPagos extends RepositorioGenerico<Pago, Integer> {
+}
